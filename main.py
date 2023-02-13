c18f752826d3c830df516d806fe326e8dcb02250
klean2050 <k.avramidis@windowslive.com>
2023-02-13 12:20:10 -0800

    upd sample experiment

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import os
 from scipy import stats
 from process import FixationAnalyzer, Subject
-from utils import DATA_ROOT
+from utils import DATA_ROOT, TRIAL_LIST
 
 if __name__ == "__main__":
-    root = DATA_ROOT
-    trial = "Freeviewingstillimage_1.jpg"
 
     ids = [i for i in os.listdir(DATA_ROOT) if i.endswith(".asc")]
     ctrl_ids = [i.split(".")[0] for i in ids if i.split("_")[0].startswith("2")]
@@ -16,21 +14,27 @@ if __name__ == "__main__":
     # SAMPLE EXPERIMENT #
     # # # # # # # # # # #
 
-    durations_ctrl = []
-    for subject in ctrl_ids:
-        sub = Subject(DATA_ROOT, subject)
-        out = sub.extract_fixations(trial_name=trial)
-        fix_analyzer = FixationAnalyzer(DATA_ROOT, out)
-        dur = fix_analyzer.number_of_fixations()
-        durations_ctrl.append(dur)
+    for trial in TRIAL_LIST:  
+        features_ctrl = []
+        for subject in ctrl_ids:
+            sub = Subject(DATA_ROOT, subject)
+            out = sub.extract_fixations(trial_name=trial)
+            fix_analyzer = FixationAnalyzer(DATA_ROOT, out)
+            dur = fix_analyzer.number_of_fixations()
+            features_ctrl.append(dur)
 
-    durations_cvi = []
-    for subject in cvi_ids:
-        sub = Subject(DATA_ROOT, subject)
-        out = sub.extract_fixations(trial_name=trial)
-        fix_analyzer = FixationAnalyzer(DATA_ROOT, out)
-        dur = fix_analyzer.number_of_fixations()
-        durations_cvi.append(dur)
+        features_cvi = []
+        for subject in cvi_ids:
+            sub = Subject(DATA_ROOT, subject)
+            out = sub.extract_fixations(trial_name=trial)
+            fix_analyzer = FixationAnalyzer(DATA_ROOT, out)
+            dur = fix_analyzer.number_of_fixations()
+            features_cvi.append(dur)
 
-    stat, p_value = stats.ttest_ind(durations_ctrl, durations_cvi)
-    print(p_value)
+        stat, p_value = stats.ttest_ind(features_ctrl, features_cvi)
+        significance = (
+            "***" if p_value < 0.001 else
+            "**" if p_value < 0.01 else
+            "*" if p_value < 0.05 else ""
+        )
+        print(trial, p_value, significance)
