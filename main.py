589a00dc3f5768cb5eb921b7a588405686610304
klean2050 <k.avramidis@windowslive.com>
2023-02-14 18:29:50 -0800

    testing p values

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ if __name__ == "__main__":
     # SAMPLE EXPERIMENT #
     # # # # # # # # # # #
 
-    for trial in TRIAL_LIST:  
+    for trial in TRIAL_LIST:
         features_ctrl = []
         for subject in ctrl_ids:
             sub = Subject(DATA_ROOT, subject)
             out = sub.extract_fixations(trial_name=trial)
             fix_analyzer = FixationAnalyzer(DATA_ROOT, out)
-            dur = fix_analyzer.number_of_fixations()
+            dur, _ = fix_analyzer.duration_of_fixations()
             features_ctrl.append(dur)
 
         features_cvi = []
@@ -28,13 +28,17 @@ if __name__ == "__main__":
             sub = Subject(DATA_ROOT, subject)
             out = sub.extract_fixations(trial_name=trial)
             fix_analyzer = FixationAnalyzer(DATA_ROOT, out)
-            dur = fix_analyzer.number_of_fixations()
+            dur, _ = fix_analyzer.duration_of_fixations()
             features_cvi.append(dur)
 
         stat, p_value = stats.ttest_ind(features_ctrl, features_cvi)
         significance = (
-            "***" if p_value < 0.001 else
-            "**" if p_value < 0.01 else
-            "*" if p_value < 0.05 else ""
+            "***"
+            if p_value < 0.001
+            else "**"
+            if p_value < 0.01
+            else "*"
+            if p_value < 0.05
+            else ""
         )
         print(trial, p_value, significance)
