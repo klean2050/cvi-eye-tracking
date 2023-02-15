2f55b757b097012f7ce7b5bff30d13e148837289
klean2050 <k.avramidis@windowslive.com>
2023-02-15 02:34:49 -0800

    testing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from scipy import stats
-from process import FixationAnalyzer, Subject
+from process import *
 from utils import DATA_ROOT, TRIAL_LIST
 
 if __name__ == "__main__":
@@ -14,24 +14,28 @@ if __name__ == "__main__":
     # SAMPLE EXPERIMENT #
     # # # # # # # # # # #
 
+    p_values = []
     for trial in TRIAL_LIST:
+        this_trial = ImageTrial(DATA_ROOT, trial, "smaps")
+        a = this_trial.load_saliency_map(0).T[0]
+
         features_ctrl = []
         for subject in ctrl_ids:
             sub = Subject(DATA_ROOT, subject)
             out = sub.extract_fixations(trial_name=trial)
             fix_analyzer = FixationAnalyzer(DATA_ROOT, out)
-            dur, _ = fix_analyzer.duration_of_fixations()
-            features_ctrl.append(dur)
+            feat = fix_analyzer.average_saliency(a)
+            features_ctrl.append(feat)
 
         features_cvi = []
         for subject in cvi_ids:
             sub = Subject(DATA_ROOT, subject)
             out = sub.extract_fixations(trial_name=trial)
             fix_analyzer = FixationAnalyzer(DATA_ROOT, out)
-            dur, _ = fix_analyzer.duration_of_fixations()
-            features_cvi.append(dur)
+            feat = fix_analyzer.average_saliency(a)
+            features_cvi.append(feat)
 
-        stat, p_value = stats.ttest_ind(features_ctrl, features_cvi)
+        stat, p_value = stats.ttest_ind(features_ctrl, features_cvi, equal_var=False)
         significance = (
             "***"
             if p_value < 0.001
@@ -42,3 +46,10 @@ if __name__ == "__main__":
             else ""
         )
         print(trial, p_value, significance)
+        p_values.append(p_value)
+
+    finalp = stats.combine_pvalues(p_values)[1]
+    print("\nAfter Bonferroni correction for 55 stimuli,")
+    print(f"the significant level was set to p < 0.0002,")
+    print("and the combined p-value (Fisher's method)")
+    print("was found at p =", finalp)
